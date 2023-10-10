f15b3bdd4fc195bd19769e491a3f88fb7188d219
ImgyeongLee <mingbab33@gmail.com>
2023-10-09 21:47:16 -0700

    Database connect

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+
+config();
 
 const PORT = process.env.PORT || 8000;
-
 const app = express();
+const mongoURI = process.env.MONGO_URI;
 
-// TODO: mongoose!
-
-app.listen(PORT, () => {
-  console.log("Server is listening at PORT ", PORT);
-});
+// TODO: Study each line! and design the data structure
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log(`Database Connection failed: ${err}`);
+  })
+  .finally(() => {
+    app.listen(PORT, () => {
+      console.log("Server is listening at PORT ", PORT);
+    });
+  });
